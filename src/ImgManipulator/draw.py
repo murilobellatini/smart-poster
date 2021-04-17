63c9c20b54fb41bb673bf2413dd2888a6f766736
Murilo <43999445+murilobellatini@users.noreply.github.com>
2021-04-17 19:33:24 -0300

    Implement text drawing

---
+++ src/ImgManipulator/draw.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+from src.paths import LOCAL_RAW_DATA_PATH
+
+
+def draw_text(txt, font='Poppins-Bold.otf', fontsize=35, fontcolor_hex="black", export=False, format="PNG"):
+    font = ImageFont.truetype(font, fontsize)
+    fontcolor = ImageColor.getcolor(fontcolor_hex, "RGB")
+
+    filename = txt.replace('\n', '-')
+
+    im_dummy = Image.new("RGBA", (1, 1))
+    draw = ImageDraw.Draw(im_dummy)
+    W, H = draw.textsize(txt, font)
+
+    im = Image.new("RGBA", (W, H))
+    draw = ImageDraw.Draw(im)
+    w, h = draw.textsize(txt, font)
+
+    draw.text(((W-w)/2, (H-h)/2), text=txt, fill=fontcolor, font=font)
+
+    if export:
+        im.save(LOCAL_RAW_DATA_PATH / f"{filename}.{format}", format)
+
+    return im
