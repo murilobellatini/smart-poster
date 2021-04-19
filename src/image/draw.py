21bccf213faf4e62ae237e94ade4b1e3fbd892aa
Murilo <43999445+murilobellatini@users.noreply.github.com>
2021-04-19 20:43:41 -0300

    Explore empty space detection

--- src/image/draw.py
+++ src/image/draw.py
@@ -28,14 +28,18 @@ def draw_text(txt, font='Poppins-Bold.otf', fontsize=35, fontcolor_hex="black", 
 def draw_boxes(img:np.ndarray, classIds:np.ndarray, classNames:list, confs:np.ndarray, bbox:np.ndarray):
     
     img_boxed = img.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     output_rectangles = np.zeros(img_boxed.shape, dtype="uint8")
     
     for classId, confidence,box in zip(classIds.flatten(), confs.flatten(), bbox):
         cv2.rectangle(img_boxed,box,color=(0,255,0),thickness=2)
+        cv2.rectangle(gray,box,color=(0,0,0),thickness=cv2.FILLED)
         rectangle = cv2.rectangle(output_rectangles,box,color=(255,255,255),thickness=cv2.FILLED)
         output_rectangles = cv2.bitwise_or(output_rectangles, rectangle)
         cv2.putText(img_boxed,classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
         cv2.putText(img_boxed,str(round(confidence*100,2)),(box[0]+200,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            
-    return img_boxed, output_rectangles
+    
+
+
+    return img_boxed, gray
