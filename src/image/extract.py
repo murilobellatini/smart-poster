e2438ef158ae951aebf43ac52490aac482306dc9
Murilo <43999445+murilobellatini@users.noreply.github.com>
2021-04-20 10:05:12 -0300

    Generalize Image Extractor

--- src/image/extract.py
+++ src/image/extract.py
@@ -11,24 +11,65 @@ google_key = os.environ.get('GCP_KEY')
 google_cx = os.environ.get('GCX')
 
 # instantiate image search objects
-unsplash = PyUnsplash(api_key=unsplash_key)
-gis = GoogleImagesSearch(google_key, google_cx)
+api_instances = {
+    'unsplash': PyUnsplash(api_key=unsplash_key),
+    'google': GoogleImagesSearch(google_key, google_cx)
+}
 
 
-def search(query: str, type_: str = 'photos', api:str="unsplash", min_results:int=10):
-    """
-    Searchs `type_` data (`photos`, `collections` or `users`)
-    based on `query` via `api`
-    """
-    if api == "unsplash":
-        return unsplash.search(type_=type_, query=query)
-    elif api == 'google':
+class ApiImgExtractor():
+
+    def __init__(self, api: str):
+
+        if api not in api_instances.keys():
+            raise NotImplementedError
+
+        self.api = api
+        self.api_instance = api_instances[self.api]
+        self.img_urls = set()  # all found image urls (accumulated)
+        self.curr_img_urls = set()  # image urls from current pagination only
+
+    def query(self, _search_params: dict):
+        """
         _search_params = {
-            'q': query,
-            'imgType': type_,
-            'num': min_results
+            'q': '...',
+            'num': 10,
+            'safe': 'high|medium|off',
+            'fileType': 'jpg|gif|png',
+            'imgType': google -> 'clipart|face|lineart|news|photo' / unsplash -> 'photos|colletions|users'
+            'imgSize': 'huge|icon|large|medium|small|xlarge|xxlarge',
+            'imgDominantColor': 'black|blue|brown|gray|green|pink|purple|teal|white|yellow',
+            'rights': 'cc_publicdomain|cc_attribute|cc_sharealike|cc_noncommercial|cc_nonderived'
         }
-        gis.search(search_params=_search_params)
-        return gis
-    else:
-        raise NotImplementedError
+        """
+        if self.api == 'unsplash':
+            self.cursor = self.api_instance.search(
+                type_=_search_params['imgType'], query=_search_params['q'])  # @todo: generalize for `collections` and `users`
+            self._update_img_urls()
+
+        elif self.api == 'google':
+            self.api_instance.search(
+                search_params=_search_params)
+            self.cursor = self.api_instance.results()
+            self._update_img_urls()
+
+    def _update_img_urls(self):
+
+        if self.api == 'unsplash':
+            self.curr_img_urls = set(r['urls']['full']
+                                     for r in self.cursor.body['results'])
+
+        if self.api == 'google':
+            self.curr_img_urls = set(im.url
+                                     for im in self.cursor)
+
+        self.img_urls.update(self.curr_img_urls)
+
+    def paginate_results(self):
+        if self.api == 'unsplash':
+            self.cursor = self.cursor.get_next_page()
+            self._update_img_urls()
+        if self.api == 'google':
+            self.api_instance.next_page()
+            self.cursor = self.api_instance.results()
+            self._update_img_urls()
