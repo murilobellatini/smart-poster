779abc5ef021192237e2183a7da5cf5e8ae45deb
Murilo <43999445+murilobellatini@users.noreply.github.com>
2021-04-22 14:22:00 -0300

    Wrap Quote within TextExtractor

--- src/text/extract.py
+++ src/text/extract.py
@@ -1,14 +1,15 @@
 
 from __future__ import print_function
 
-from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
-import gensim
+from nltk.corpus import stopwords
 from gensim import corpora
-import string
-import argparse
-import re
 from nltk import download
+from numpy import source
+from quote import quote
+import gensim
+import string
+import re
 
 download('stopwords')
 download('wordnet')
@@ -23,42 +24,78 @@ regex = re.compile(
     r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
-def generate_hashtags(text, lang: str = 'english', num_topics: int = 10, passes: int = 3):
+class QuoteExtractor():
 
-    stop = set(stopwords.words(lang))
-    exclude = set(string.punctuation)
-    lemma = WordNetLemmatizer()
+    def __init__(self, query: str, ext_source: str = 'QUOTE_API', limit: int = 10):
 
-    doc_complete = text.split('\n')
-
-    def clean(doc):
-        stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-        punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
-        normalized = " ".join(lemma.lemmatize(word)
-                              for word in punc_free.split())
-        return normalized
-
-    doc_clean = [clean(doc).split() for doc in doc_complete]
-    dictionary = corpora.Dictionary(doc_clean)
-    doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
-    Lda = gensim.models.ldamodel.LdaModel
-    ldamodel = Lda(doc_term_matrix, num_topics=num_topics,
-                   id2word=dictionary, passes=passes)
-    topic = ldamodel.print_topics(num_topics=5, num_words=5)
-
-    hashtags = []
-    for t in topic:
-        for h in t[1].split('+'):
-            hashtags.append('#'+h[h.find('"')+1:h.rfind('"')])
-
-    return list(set(hashtags))
+        if ext_source == 'QUOTE_API':
+            results = []
+            for r in quote(search=query, limit=limit):
+                q = Quote(
+                    author=r.get('author'),
+                    quote=r.get('quote'),
+                    source=r.get('book'),
+                    source_type='book'
+                )
+                results.append(q)
+            self.results = results
+        else:
+            raise NotImplementedError
 
 
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description='Python script to extract hashtags from URL or long text based on LDA model from GenSim')
-#     parser.add_argument('--document', type=str, required=True, default='', help='Path or URL to the document')
-#     parser.add_argument('--language', type=str, required=False, default='english', help='Language of the text')
-#     parser.add_argument('--hashtags', type=int, required=False, default=4, help='Number of hashtags to extract')
-#     parser.add_argument('--passes', type=int, required=False, default=50, help='Iteration for the LDA model to perform')
-#     FLAGS = parser.parse_args()
-#     main()
+class Quote():
+
+    def __init__(self, quote: str, author: str, source: str, source_type: str, lang: str = 'english'):
+        self.quote = quote
+        self.author = author
+        self.source = source
+        self.source_type = source_type
+        self.lang = lang
+        self.hashtags = self.generate_hashtags()
+
+    def __repr__(self):
+        return f"<Quote author:`{self.author}` source:`{self.source}` quote_prev:`{self.quote[:15]}...`>"
+
+    def to_dict(self):
+        return {
+            'quote': self.quote,
+            'author': self.author,
+            'source': self.source,
+            'source_type': self.source_type,
+            'lang': self.lang,
+        }
+
+    def generate_hashtags(self, num_topics: int = 10, passes: int = 3) -> list:
+
+        text = ' '.join(self.to_dict().values())
+
+        stop = set(stopwords.words(self.lang))
+        exclude = set(string.punctuation)
+        lemma = WordNetLemmatizer()
+
+        doc_complete = text.split('\n')
+
+        def clean(doc):
+            stop_free = " ".join(
+                [i for i in doc.lower().split() if i not in stop])
+            punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+            normalized = " ".join(lemma.lemmatize(word)
+                                  for word in punc_free.split())
+            return normalized
+
+        doc_clean = [clean(doc).split() for doc in doc_complete]
+        dictionary = corpora.Dictionary(doc_clean)
+        doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+        Lda = gensim.models.ldamodel.LdaModel
+        ldamodel = Lda(doc_term_matrix, num_topics=num_topics,
+                       id2word=dictionary, passes=passes)
+        topic = ldamodel.print_topics(num_topics=5, num_words=5)
+
+        hashtags = []
+        for t in topic:
+            for h in t[1].split('+'):
+                hashtags.append('#'+h[h.find('"')+1:h.rfind('"')])
+
+        self.hashtags = list(set(hashtags))
+
+        return self.hashtags
